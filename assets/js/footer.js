ca65390aef010c76762bbe728eb78631ae13b3fb
Kartikey Tyagi <kartikeytyagi@Kartikeys-MacBook-Pro.local>
2024-07-01 11:46:55 +0530

    Base code

---
+++ assets/js/footer.js
@@ -0,0 +1,107 @@
+document.getElementById("global-footer").innerHTML += `
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <img
+            src="../assets/images/logo-full.png"
+            alt="Best website development company in delhi NCR"
+            class="img-fluid"
+            style="max-width: 150px"
+          />
+          <p>
+            Delhi, NCR <br />
+            ( Serves Pan India)<br />
+
+            <strong>Phone:</strong> +91 8171500215<br />
+            <strong>Email:</strong> bubblebursttech@gmail.com<br />
+          </p>
+        </div>
+
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html">Home</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#about">About us</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Services</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../terms-of-service.html">Terms of service</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../privacy-policy.html">Privacy policy</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Our Services</h4>
+          <ul>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Web Design</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Web Development</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Social Media Management</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Video Editing</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="../index.html#services">Graphic Design</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-4 col-md-6 footer-newsletter">
+          <h4>Join Our Newsletter</h4>
+          <p>Subscribe and get free tips/tricks to build your brand.</p>
+          <form
+            action="https://formsubmit.co/bubblebursttech@gmail.com"
+            method="post"
+            enctype="multipart/form-data"
+          >
+            <input type="email" name="email" /><input
+              type="submit"
+              value="Subscribe"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container d-md-flex py-4">
+    <div class="me-md-auto text-center text-md-start">
+      <div class="copyright">
+        &copy; Copyright
+        <strong><span>Bubble Burst Technologies</span></strong
+        >. <br />
+        All Rights Reserved
+      </div>
+    </div>
+    <div class="social-links text-center text-md-right pt-3 pt-md-0">
+    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+  </div>
+</footer>`;
---
+++ assets/js/header.js
@@ -0,0 +1,45 @@
+document.getElementById(
+  "global-navbar"
+).innerHTML += `    <header id="header" class="fixed-top">
+<div class="container d-flex align-items-center justify-content-between">
+  <a href="../index.html" class="logo"
+    ><img
+      src="../assets/images/logo-full.png"
+      alt="Best website development company in delhi NCR"
+      class="img-fluid"
+  /></a>
+
+  <nav id="navbar" class="navbar">
+    <ul>
+      <li><a class="nav-link scrollto" href="../index.html">Home</a></li>
+      <li><a class="nav-link scrollto" href="../about-bubble-burst-technologies/about-us.html">About</a></li>
+      <li><a class="nav-link scrollto" href="../index.html#services">Services</a></li>
+      <li>
+        <a class="nav-link scrollto" href="../index.html#portfolio">Clients</a>
+      </li>
+
+      <li>
+      <a
+        class="nav-link"
+        href="../case-studies-bubble-burst-technologies/case-study-main.html"
+        >Case Study</a
+      >
+    </li>
+    <li>
+    <a class="nav-link scrollto" href="../blogs/blogs-main.html"
+      >Blogs</a
+    >
+  </li>
+  <li>
+    <a
+      class="getstarted scrollto"
+      href="../contact-bubble-burst-technologies/contact-us.html"
+      >Contact</a
+    >
+  </li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+  <!-- .navbar -->
+</div>
+</header>`;
