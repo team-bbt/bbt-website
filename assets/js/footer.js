e8fbd6bef1640390f01ca0974fed330834165f84
Kartikey Tyagi <kartikeytyagi@Kartikeys-MacBook-Pro.local>
2024-09-16 09:04:21 +0530

    minor changes

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -98,10 +98,5 @@ document.getElementById("global-footer").innerHTML += `
         All Rights Reserved
       </div>
     </div>
-    <div class="social-links text-center text-md-right pt-3 pt-md-0">
-    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-  </div>
   </div>
 </footer>`;
